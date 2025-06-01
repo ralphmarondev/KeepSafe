74d4591b61c023c933a209194ff3e06b8a9089e5
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-01 15:48:46 +0800

    initial commit

---
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/App.kt
@@ -0,0 +1,54 @@
+package com.ralphmarondev.keepsafe
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun App() {
+    MaterialTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "KeepSafe"
+                        )
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary
+                    )
+                )
+            }
+        ) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Hello there, Ralph Maron Eda is here!",
+                    fontFamily = MaterialTheme.typography.titleLarge.fontFamily,
+                    fontWeight = MaterialTheme.typography.titleLarge.fontWeight,
+                    color = MaterialTheme.colorScheme.secondary,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
---
+++ composeApp/src/commonTest/kotlin/com/ralphmarondev/keepsafe/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.ralphmarondev.keepsafe
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
