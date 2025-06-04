a0e2642b214460cd2d84440fa6d63ce460b2e59b
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-04 10:05:46 +0800

    added fonts but is not working as of now lol

---
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/core/theme/Type.kt
@@ -0,0 +1,37 @@
+package com.ralphmarondev.keepsafe.core.theme
+
+// 2025-06-04
+// NOTE: THIS IS NOT WORKING LOL
+
+
+//val robotoMonoRegular = FontFamily(
+//    Font(Res.font.roboto_mono_regular)
+//)
+//
+//val robotoMonoBold = FontFamily(
+//    Font(Res.font.roboto_mono_bold)
+//)
+//
+//val robotoMonoThin = FontFamily(
+//    Font(Res.font.roboto_mono_thin)
+//)
+//
+//val baseline = Typography()
+//
+//val Typography = Typography(
+//    displayLarge = baseline.displayLarge.copy(fontFamily = robotoMonoBold),
+//    displayMedium = baseline.displayMedium.copy(fontFamily = robotoMonoBold),
+//    displaySmall = baseline.displaySmall.copy(fontFamily = robotoMonoBold),
+//    headlineLarge = baseline.headlineLarge.copy(fontFamily = robotoMonoBold),
+//    headlineMedium = baseline.headlineMedium.copy(fontFamily = robotoMonoBold),
+//    headlineSmall = baseline.headlineSmall.copy(fontFamily = robotoMonoBold),
+//    titleLarge = baseline.titleLarge.copy(fontFamily = robotoMonoRegular),
+//    titleMedium = baseline.titleMedium.copy(fontFamily = robotoMonoRegular),
+//    titleSmall = baseline.titleSmall.copy(fontFamily = robotoMonoRegular),
+//    bodyLarge = baseline.bodyLarge.copy(fontFamily = robotoMonoRegular),
+//    bodyMedium = baseline.bodyMedium.copy(fontFamily = robotoMonoRegular),
+//    bodySmall = baseline.bodySmall.copy(fontFamily = robotoMonoRegular),
+//    labelLarge = baseline.labelLarge.copy(fontFamily = robotoMonoRegular),
+//    labelMedium = baseline.labelMedium.copy(fontFamily = robotoMonoRegular),
+//    labelSmall = baseline.labelSmall.copy(fontFamily = robotoMonoRegular),
+//)
