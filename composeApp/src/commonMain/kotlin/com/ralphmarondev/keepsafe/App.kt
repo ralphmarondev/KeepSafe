d47ed2023732bd083da7c074e4d8208fd82198fb
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-04 09:43:32 +0800

    updated color theme

--- composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/App.kt
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/App.kt
@@ -1,13 +1,13 @@
 package com.ralphmarondev.keepsafe
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.ralphmarondev.keepsafe.core.theme.KeepSafeTheme
 import com.ralphmarondev.keepsafe.navigation.AppNavigation
 import org.koin.compose.KoinContext
 
 @Composable
 fun App() {
-    MaterialTheme {
+    KeepSafeTheme {
         KoinContext {
             AppNavigation()
         }
