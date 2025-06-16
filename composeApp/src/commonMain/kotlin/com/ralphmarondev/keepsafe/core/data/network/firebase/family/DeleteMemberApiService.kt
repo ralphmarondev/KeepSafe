c3aef06c98e906def925371a5ba6bf841b783841
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-16 20:40:08 +0800

    fix soft delete removing all fields

--- composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/core/data/network/firebase/family/DeleteMemberApiService.kt
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/core/data/network/firebase/family/DeleteMemberApiService.kt
@@ -4,14 +4,21 @@ import com.ralphmarondev.keepsafe.core.util.Secrets
 import io.ktor.client.HttpClient
 import io.ktor.client.request.patch
 import io.ktor.client.request.setBody
+import io.ktor.http.appendPathSegments
 import io.ktor.http.isSuccess
+import io.ktor.http.takeFrom
 
 class DeleteMemberApiService(
     private val httpClient: HttpClient
 ) {
     suspend fun delete(uid: String): Boolean {
         return try {
-            val response = httpClient.patch("${Secrets.userDetailsUrl}/$uid") {
+            val response = httpClient.patch {
+                url {
+                    takeFrom(Secrets.userDetailsUrl)
+                    appendPathSegments(uid)
+                    parameters.append("updateMask.fieldPaths", "isDeleted")
+                }
                 setBody(
                     mapOf(
                         "fields" to mapOf(
