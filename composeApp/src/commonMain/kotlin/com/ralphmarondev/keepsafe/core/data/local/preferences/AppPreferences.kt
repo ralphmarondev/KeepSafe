5904d2e49485e62fa2561f051174cefcf073ae7e
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-16 23:15:51 +0800

    clean AppPreferences

--- composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/core/data/local/preferences/AppPreferences.kt
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/core/data/local/preferences/AppPreferences.kt
@@ -8,7 +8,6 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
 import okio.Path.Companion.toPath
 
 class AppPreferences(
@@ -16,9 +15,6 @@ class AppPreferences(
 ) {
     companion object {
         private val DARK_THEME_KEY = booleanPreferencesKey("dark_theme_key")
-        private val ID_TOKEN_KEY = stringPreferencesKey("id_token")
-        private val REFRESH_TOKEN_KEY = stringPreferencesKey("refresh_token")
-        private val LOCAL_ID_KEY = stringPreferencesKey("local_id")
         private val EMAIL_KEY = stringPreferencesKey("email")
         private val FAMILY_ID_KEY = stringPreferencesKey("family_id")
         private val FULL_NAME_KEY = stringPreferencesKey("full_name")
@@ -71,45 +67,6 @@ class AppPreferences(
         }
     }
 
-    fun saveAuthTokens(
-        idToken: String,
-        refreshToken: String?,
-        localId: String,
-        email: String,
-        familyId: String,
-        fullName: String,
-        role: String
-    ) {
-        runBlocking {
-            dataStore.edit {
-                it[ID_TOKEN_KEY] = idToken
-                refreshToken?.let { token -> it[REFRESH_TOKEN_KEY] = token }
-                it[LOCAL_ID_KEY] = localId
-                it[EMAIL_KEY] = email
-                it[FAMILY_ID_KEY] = familyId
-                it[FULL_NAME_KEY] = fullName
-                it[ROLE_KEY] = role
-            }
-        }
-    }
-
-    fun clearAuthTokens() {
-        runBlocking {
-            dataStore.edit {
-                it.remove(ID_TOKEN_KEY)
-                it.remove(REFRESH_TOKEN_KEY)
-                it.remove(LOCAL_ID_KEY)
-                it.remove(EMAIL_KEY)
-                it.remove(FAMILY_ID_KEY)
-                it.remove(FULL_NAME_KEY)
-                it.remove(ROLE_KEY)
-            }
-        }
-    }
-
-    fun idToken(): Flow<String?> = dataStore.data.map { it[ID_TOKEN_KEY] }
-    fun refreshToken(): Flow<String?> = dataStore.data.map { it[REFRESH_TOKEN_KEY] }
-    fun localId(): Flow<String?> = dataStore.data.map { it[LOCAL_ID_KEY] }
     fun email(): Flow<String?> = dataStore.data.map { it[EMAIL_KEY] }
     fun familyId(): Flow<String?> = dataStore.data.map { it[FAMILY_ID_KEY] }
     fun fullName(): Flow<String?> = dataStore.data.map { it[FULL_NAME_KEY] }
