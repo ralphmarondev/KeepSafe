4dee905090ab970a3d2f497a08b593111e72ce50
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-18 13:52:01 +0800

    update FamilyMemberCard

--- composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/family/presentation/components/FamilyMemberCard.kt
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/family/presentation/components/FamilyMemberCard.kt
@@ -1,18 +1,28 @@
 package com.ralphmarondev.keepsafe.family.presentation.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.ralphmarondev.keepsafe.family.domain.model.FamilyMember
+import keepsafe.composeapp.generated.resources.Res
+import keepsafe.composeapp.generated.resources.img
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun FamilyMemberCard(
@@ -28,8 +38,18 @@ fun FamilyMemberCard(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Image(
+                painter = painterResource(Res.drawable.img),
+                contentDescription = "Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
             Column {
                 val isCurrentUser = familyMember.uid == currentUserUid
                 val fullName = familyMember.fullName ?: "No fullName provided."
@@ -48,8 +68,8 @@ fun FamilyMemberCard(
                 Spacer(modifier = Modifier.height(2.dp))
                 Text(
                     text = familyMember.role ?: "No role provided",
-                    fontSize = MaterialTheme.typography.titleMedium.fontSize,
-                    fontWeight = MaterialTheme.typography.titleSmall.fontWeight,
+                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                    fontWeight = MaterialTheme.typography.bodyMedium.fontWeight,
                     color = MaterialTheme.colorScheme.secondary
                 )
             }
--- composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/family/presentation/member_list/FamilyMemberListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ralphmarondev/keepsafe/family/presentation/member_list/FamilyMemberListScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -101,7 +100,6 @@ fun FamilyMemberListScreen(
                 items(familyMember) {
                     FamilyMemberCard(
                         modifier = Modifier
-                            .widthIn(max = 500.dp)
                             .fillMaxWidth()
                             .padding(vertical = 4.dp),
                         familyMember = it,
