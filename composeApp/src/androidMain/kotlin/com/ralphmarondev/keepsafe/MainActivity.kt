a5cd2b51c946344b6cdf0bc89da7013c167a8a02
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-24 11:40:30 +0800

    fix status bar color issue on android

--- composeApp/src/androidMain/kotlin/com/ralphmarondev/keepsafe/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/ralphmarondev/keepsafe/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.ralphmarondev.keepsafe
 
+import android.app.Activity
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import com.ralphmarondev.keepsafe.core.theme.ThemeState
 import org.koin.android.ext.android.get
 
@@ -17,6 +22,19 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val themeState: ThemeState = get()
+            val darkMode = themeState.darkMode.collectAsState().value
+            val view = LocalView.current
+
+            if (!view.isInEditMode) {
+                SideEffect {
+                    val window = (view.context as Activity).window
+                    val insetsController = window?.let {
+                        WindowCompat.getInsetsController(it, view)
+                    }
+                    insetsController?.isAppearanceLightStatusBars = !darkMode
+                }
+            }
+
             App(themeState = themeState)
         }
     }
