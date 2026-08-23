92a667bec717e83b12e2da5231b5979779f81073
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-08-23 13:07:12 +0800

    android: update reusable component

--- android/src/main/kotlin/com/ralphmarondev/keepsafe/core/presentation/component/KOutlinedButton.kt
+++ android/src/main/kotlin/com/ralphmarondev/keepsafe/core/presentation/component/KOutlinedButton.kt
@@ -1,16 +1,43 @@
 package com.ralphmarondev.keepsafe.core.presentation.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun KOutlinedButton(
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = RoundedCornerShape(8.dp),
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    elevation: ButtonElevation? = null,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = PaddingValues(),
+    interactionSource: MutableInteractionSource? = null,
+    content: @Composable (RowScope.() -> Unit)
 ) {
     OutlinedButton(
         onClick = onClick,
-        modifier = modifier
-    ) { }
+        modifier = modifier,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        elevation = elevation,
+        border = border,
+        contentPadding = contentPadding,
+        interactionSource = interactionSource
+    ) {
+        content()
+    }
 }
--- android/src/main/kotlin/com/ralphmarondev/keepsafe/core/presentation/component/KPasswordField.kt
+++ android/src/main/kotlin/com/ralphmarondev/keepsafe/core/presentation/component/KPasswordField.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun KTextField(
+fun KPasswordField(
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
