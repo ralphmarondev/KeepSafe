691df18a9583d867a9288e75e92ceaa76e18422b
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-08-25 14:25:04 +0800

    android: fix saving family, account and member issue on firestore

--- android/src/main/kotlin/com/ralphmarondev/keepsafe/feature/auth/presentation/register/RegisterViewModel.kt
+++ android/src/main/kotlin/com/ralphmarondev/keepsafe/feature/auth/presentation/register/RegisterViewModel.kt
@@ -3,12 +3,19 @@ package com.ralphmarondev.keepsafe.feature.auth.presentation.register
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ralphmarondev.keepsafe.domain.model.Account
+import com.ralphmarondev.keepsafe.domain.model.Family
+import com.ralphmarondev.keepsafe.domain.model.Member
+import com.ralphmarondev.keepsafe.domain.model.Result
+import com.ralphmarondev.keepsafe.domain.repository.AuthRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class RegisterViewModel : ViewModel() {
+class RegisterViewModel(
+    private val authRepository: AuthRepository
+) : ViewModel() {
 
     private val _state = MutableStateFlow(RegisterState())
     val state = _state.asStateFlow()
@@ -167,63 +174,119 @@ class RegisterViewModel : ViewModel() {
 
     private fun register() {
         viewModelScope.launch {
-            val username = _state.value.username.trim().lowercase()
-            val password = _state.value.password.trim()
-            val confirmPassword = _state.value.confirmPassword.trim()
-            var isValid = true
+            try {
+                val username = _state.value.username.trim().lowercase()
+                val password = _state.value.password.trim()
+                val confirmPassword = _state.value.confirmPassword.trim()
+                var isValid = true
 
-            _state.update {
-                it.copy(
-                    usernameError = false,
-                    usernameErrorMessage = null,
-                    passwordError = false,
-                    passwordErrorMessage = null,
-                    confirmPasswordError = false,
-                    confirmPasswordErrorMessage = null
-                )
-            }
+                if (state.value.isLoading)
+                    return@launch
 
-            if (username.isBlank()) {
-                isValid = false
                 _state.update {
                     it.copy(
-                        usernameError = true,
-                        usernameErrorMessage = "Username cannot be empty."
+                        usernameError = false,
+                        usernameErrorMessage = null,
+                        passwordError = false,
+                        passwordErrorMessage = null,
+                        confirmPasswordError = false,
+                        confirmPasswordErrorMessage = null,
+                        isLoading = true,
+                        message = null,
+                        showMessage = false,
+                        isRegistered = false
                     )
                 }
-            } else if (!username.matches(Regex("^[a-z0-9]+$"))) {
-                isValid = false
+
+                if (username.isBlank()) {
+                    isValid = false
+                    _state.update {
+                        it.copy(
+                            usernameError = true,
+                            usernameErrorMessage = "Username cannot be empty."
+                        )
+                    }
+                } else if (!username.matches(Regex("^[a-z0-9]+$"))) {
+                    isValid = false
+                    _state.update {
+                        it.copy(
+                            usernameError = true,
+                            usernameErrorMessage = "Username can only contain letters and numbers."
+                        )
+                    }
+                }
+
+                if (password.isBlank()) {
+                    isValid = false
+                    _state.update {
+                        it.copy(
+                            passwordError = true,
+                            passwordErrorMessage = "Password cannot be empty."
+                        )
+                    }
+                }
+
+                if (password != confirmPassword) {
+                    isValid = false
+                    _state.update {
+                        it.copy(
+                            confirmPasswordError = true,
+                            confirmPasswordErrorMessage = "Password not matched."
+                        )
+                    }
+                }
+
+                if (isValid) {
+                    val family = Family(
+                        code = _state.value.familyCode.trim(),
+                        name = _state.value.familyName.trim()
+                    )
+                    val member = Member(
+                        familyCode = _state.value.familyCode.trim(),
+                        firstName = _state.value.firstName.trim(),
+                        lastName = _state.value.lastName.trim(),
+                        middleName = _state.value.middleName.trim(),
+                        maidenName = _state.value.maidenName.trim()
+                    )
+                    val account = Account(
+                        username = _state.value.username.trim(),
+                        password = _state.value.password.trim()
+                    )
+                    val result = authRepository.register(
+                        family = family,
+                        member = member,
+                        account = account
+                    )
+
+                    when (result) {
+                        is Result.Success -> {
+                            _state.update { it.copy(isRegistered = true) }
+                        }
+
+                        is Result.Error -> {
+                            _state.update {
+                                it.copy(
+                                    usernameError = result.message?.contains(
+                                        "Username is already taken.",
+                                        ignoreCase = true
+                                    ) == true,
+                                    usernameErrorMessage = result.message
+                                )
+                            }
+                        }
+                    }
+                }
+            } catch (e: Exception) {
                 _state.update {
                     it.copy(
-                        usernameError = true,
-                        usernameErrorMessage = "Username can only contain letters and numbers."
+                        message = "Registration failed!",
+                        showMessage = true
                     )
                 }
-            }
-
-            if (password.isBlank()) {
-                isValid = false
-                _state.update {
-                    it.copy(
-                        passwordError = true,
-                        passwordErrorMessage = "Password cannot be empty."
-                    )
-                }
-            }
-
-            if (password != confirmPassword) {
-                isValid = false
-                _state.update {
-                    it.copy(
-                        confirmPasswordError = true,
-                        confirmPasswordErrorMessage = "Password not matched."
-                    )
-                }
-            }
-
-            if (isValid) {
-                _state.update { it.copy(isRegistered = true) }
-                Log.d("Register", "Registration successful. Username: $username")
+                Log.e("Registration", "Failed. Error: ${e.message}")
+                e.printStackTrace()
+            } finally {
+                _state.update { it.copy(isLoading = false) }
             }
         }
     }
