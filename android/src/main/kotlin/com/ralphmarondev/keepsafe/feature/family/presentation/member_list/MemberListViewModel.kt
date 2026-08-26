88ce3c63cda06f27f78b1c4ed477d66d4bcef52f
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-08-26 13:05:54 +0800

    android: update MemberListViewModel

--- android/src/main/kotlin/com/ralphmarondev/keepsafe/feature/family/presentation/member_list/MemberListViewModel.kt
+++ android/src/main/kotlin/com/ralphmarondev/keepsafe/feature/family/presentation/member_list/MemberListViewModel.kt
@@ -1,15 +1,32 @@
 package com.ralphmarondev.keepsafe.feature.family.presentation.member_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ralphmarondev.keepsafe.domain.repository.MemberRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class MemberListViewModel : ViewModel() {
+class MemberListViewModel(
+    private val memberRepository: MemberRepository
+) : ViewModel() {
 
     private val _state = MutableStateFlow(MemberListState())
     val state = _state.asStateFlow()
 
+    init {
+        loadMembers()
+    }
+
     fun onAction(action: MemberListAction) {
 
+    }
+
+    private fun loadMembers(isRefreshing: Boolean = false) {
+        viewModelScope.launch {
+            val members = memberRepository.getMembers()
+            _state.update { it.copy(members = members) }
+        }
     }
 }
