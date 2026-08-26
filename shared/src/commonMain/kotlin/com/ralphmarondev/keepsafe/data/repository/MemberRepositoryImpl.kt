fe0029030446c844929f51774fc0d5a3870d21aa
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-08-26 13:23:37 +0800

    shared: add getFamilyInformation on MemberRepository

--- shared/src/commonMain/kotlin/com/ralphmarondev/keepsafe/data/repository/MemberRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/ralphmarondev/keepsafe/data/repository/MemberRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.ralphmarondev.keepsafe.data.repository
 
 import com.ralphmarondev.keepsafe.data.local.preference.AppPreferences
 import com.ralphmarondev.keepsafe.data.network.firebase.MemberService
+import com.ralphmarondev.keepsafe.domain.model.Family
 import com.ralphmarondev.keepsafe.domain.model.Member
 import com.ralphmarondev.keepsafe.domain.model.Result
 import com.ralphmarondev.keepsafe.domain.repository.MemberRepository
@@ -18,6 +19,24 @@ class MemberRepositoryImpl(
         return when (val result = memberService.getMembersByFamilyCode(familyCode)) {
             is Result.Success -> result.data
             is Result.Error -> emptyList()
+        }
+    }
+
+    override suspend fun getFamilyInformation(): Result<Family> {
+        return try {
+            val code = preferences.familyCode.first()
+                ?: return Result.Error(message = "Family code not found.")
+            val name = preferences.familyName.first()
+                ?: return Result.Error(message = "Family name not found.")
+
+            val family = Family(
+                code = code,
+                name = name
+            )
+            return Result.Success(family)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Result.Error(message = "Failed retrieving family information.", throwable = e)
         }
     }
 }
--- shared/src/commonMain/kotlin/com/ralphmarondev/keepsafe/domain/repository/MemberRepository.kt
+++ shared/src/commonMain/kotlin/com/ralphmarondev/keepsafe/domain/repository/MemberRepository.kt
@@ -1,7 +1,10 @@
 package com.ralphmarondev.keepsafe.domain.repository
 
+import com.ralphmarondev.keepsafe.domain.model.Family
 import com.ralphmarondev.keepsafe.domain.model.Member
+import com.ralphmarondev.keepsafe.domain.model.Result
 
 interface MemberRepository {
     suspend fun getMembers(): List<Member>
+    suspend fun getFamilyInformation(): Result<Family>
 }
