962ae55661e69e8c33b712fd9499ebf584661d49
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-09-02 15:44:37 +0800

    shared: fix no member is returning on member service

--- shared/src/commonMain/kotlin/com/ralphmarondev/keepsafe/data/network/firebase/MemberService.kt
+++ shared/src/commonMain/kotlin/com/ralphmarondev/keepsafe/data/network/firebase/MemberService.kt
@@ -7,50 +7,20 @@ import dev.gitlive.firebase.firestore.FirebaseFirestore
 class MemberService(
     private val firestore: FirebaseFirestore
 ) {
-    private val membersCollection = firestore
+    private fun membersCollection(familyCode: String) = firestore
+        .collection("families")
+        .document(familyCode)
         .collection("members")
-
-    suspend fun createMember(member: Member): Result<Member> {
-        return try {
-            if (member.uid.isBlank()) {
-                return Result.Error(message = "Member UID cannot be empty.")
-            }
-
-            val memberReference = membersCollection.document(member.uid)
-            memberReference.set(member)
-            Result.Success(member)
-        } catch (e: Exception) {
-            Result.Error(message = e.message ?: "Failed to create member.", throwable = e)
-        }
-    }
-
-    suspend fun getMemberById(memberUid: String): Result<Member> {
-        return try {
-            if (memberUid.isBlank()) {
-                return Result.Error(message = "Member UID cannot be empty.")
-            }
-            val document = membersCollection
-                .document(memberUid)
-                .get()
-
-            if (!document.exists) {
-                return Result.Error(message = "Member not found.")
-            }
-            val member = document.data<Member>()
-            Result.Success(member)
-        } catch (e: Exception) {
-            Result.Error(message = e.message ?: "Failed to fetch member.", throwable = e)
-        }
-    }
 
     suspend fun getMembersByFamilyCode(familyCode: String): Result<List<Member>> {
         return try {
             if (familyCode.isBlank()) {
-                return Result.Error("Family code cannot be empty.")
+                return Result.Error(
+                    message = "Family code cannot be empty."
+                )
             }
-            val querySnapshot = membersCollection
-                .where { "familyCode" equalTo familyCode }
-                .get()
+
+            val querySnapshot = membersCollection(familyCode).get()
 
             val members = querySnapshot.documents.map { doc ->
                 doc.data<Member>()
