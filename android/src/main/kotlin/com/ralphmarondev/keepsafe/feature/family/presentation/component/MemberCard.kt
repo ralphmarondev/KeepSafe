1b23eb6dbe2473e868e71bc6adc3bd3928ddf7e6
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-09-02 15:04:02 +0800

    android: update MemberCard and add image placeholders

--- android/src/main/kotlin/com/ralphmarondev/keepsafe/feature/family/presentation/component/MemberCard.kt
+++ android/src/main/kotlin/com/ralphmarondev/keepsafe/feature/family/presentation/component/MemberCard.kt
@@ -1,13 +1,24 @@
 package com.ralphmarondev.keepsafe.feature.family.presentation.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
+import com.ralphmarondev.keepsafe.R
 import com.ralphmarondev.keepsafe.domain.model.Member
 
 @Composable
@@ -23,9 +34,30 @@ fun MemberCard(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .padding(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = "${member.firstName} ${member.lastName}")
+            val imageModel = when {
+                member.imagePath.isBlank() -> R.drawable.first_child
+                else -> member.imagePath
+            }
+
+            Image(
+                painter = rememberAsyncImagePainter(imageModel),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Text(
+                text = "${member.firstName} ${member.lastName}",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Normal,
+                color = MaterialTheme.colorScheme.secondary
+            )
         }
     }
 }
