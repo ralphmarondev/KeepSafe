ac31f7a0ab3986927b0261ecc935ddf575d8898d
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-09-02 17:42:15 +0800

    shared: fix AppPreference not initialized on desktop

--- shared/src/jvmMain/kotlin/com/ralphmarondev/keepsafe/di/PlatformModule.jvm.kt
+++ shared/src/jvmMain/kotlin/com/ralphmarondev/keepsafe/di/PlatformModule.jvm.kt
@@ -6,10 +6,12 @@ import org.koin.dsl.module
 import java.io.File
 
 actual val platformModule: Module = module {
-    AppPreferences.create {
-        val userHome = System.getProperty("user.home")
-        val prefsDir = File(userHome, ".keepsafe")
-        prefsDir.mkdirs()
-        File(prefsDir, AppPreferences.DATASTORE_FILENAME).absolutePath
+    single {
+        AppPreferences.create {
+            val userHome = System.getProperty("user.home")
+            val prefsDir = File(userHome, ".keepsafe")
+            prefsDir.mkdirs()
+            File(prefsDir, AppPreferences.DATASTORE_FILENAME).absolutePath
+        }
     }
 }
